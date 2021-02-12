5322ae6f6be4ac829c51ebd75a15e4c89d734321
Viha27 <vihanga.18@itfac.mrt.ac.lk>
2021-02-12 12:05:42 +0530

    defined navigations for buttons under welcome banner

--- lib/screens/home/components/categories.dart
+++ lib/screens/home/components/categories.dart
@@ -1,32 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shop_app/routes.dart';
+import '../../time-table/calender_screen.dart';
 
 import '../../../size_config.dart';
 
 class Categories extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    List<Map<String, dynamic>> categories = [
+    /* List<Map<String, dynamic>> categories = [
       {"icon": "assets/icons/Flash Icon.svg", "text": "Attendance"},
       {"icon": "assets/icons/Bill Icon.svg", "text": "Time Table"},
       {"icon": "assets/icons/Game Icon.svg", "text": "Payments"},
       {"icon": "assets/icons/Gift Icon.svg", "text": "Results"},
       //{"icon": "assets/icons/Discover.svg", "text": "More"},
-    ];
+    ]; */
     return Padding(
       padding: EdgeInsets.all(getProportionateScreenWidth(25)),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: List.generate(
-          categories.length,
-          (index) => CategoryCard(
-            icon: categories[index]["icon"],
-            text: categories[index]["text"],
-            press: () {},
-          ),
-        ),
-      ),
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CategoryCard(
+              icon: "assets/icons/Flash Icon.svg",
+              text: "Attendance",
+              press: () {
+                Navigator.pushNamed(context, CalenderScreen.routeName);
+              },
+            ),
+            CategoryCard(
+              icon: "assets/icons/Flash Icon.svg",
+              text: "Time Table",
+              press: () {
+                Navigator.pushNamed(context, CalenderScreen.routeName);
+              },
+            ),
+            CategoryCard(
+              icon: "assets/icons/Flash Icon.svg",
+              text: "Payments",
+              press: () {
+                Navigator.pushNamed(context, CalenderScreen.routeName);
+              },
+            ),
+            CategoryCard(
+              icon: "assets/icons/Flash Icon.svg",
+              text: "Results",
+              press: () {
+                Navigator.pushNamed(context, CalenderScreen.routeName);
+              },
+            ),
+          ]),
     );
   }
 }
--- lib/screens/sign_up/components/sign_up_form.dart
+++ lib/screens/sign_up/components/sign_up_form.dart
@@ -7,7 +7,6 @@ import 'package:shop_app/screens/complete_profile/complete_profile_screen.dart';
 import '../../../constants.dart';
 import '../../../size_config.dart';
 
-
 class SignUpForm extends StatefulWidget {
   @override
   _SignUpFormState createState() => _SignUpFormState();
@@ -17,7 +16,7 @@ class _SignUpFormState extends State<SignUpForm> {
   final _formKey = GlobalKey<FormState>();
   String email;
   String password;
-  String conform_password;
+  //String conform_password;
   bool remember = false;
   final List<String> errors = [];
 
@@ -45,7 +44,7 @@ class _SignUpFormState extends State<SignUpForm> {
           SizedBox(height: getProportionateScreenHeight(30)),
           buildPasswordFormField(),
           SizedBox(height: getProportionateScreenHeight(30)),
-          buildConformPassFormField(),
+          // buildConformPassFormField(),
           FormError(errors: errors),
           SizedBox(height: getProportionateScreenHeight(40)),
           DefaultButton(
@@ -63,7 +62,7 @@ class _SignUpFormState extends State<SignUpForm> {
     );
   }
 
-  TextFormField buildConformPassFormField() {
+  /*  TextFormField buildConformPassFormField() {
     return TextFormField(
       obscureText: true,
       onSaved: (newValue) => conform_password = newValue,
@@ -94,7 +93,7 @@ class _SignUpFormState extends State<SignUpForm> {
         suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/Lock.svg"),
       ),
     );
-  }
+  } */
 
   TextFormField buildPasswordFormField() {
     return TextFormField(
